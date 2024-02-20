adca6b1f07e47940c252da18bb57f8c23a4ec447
William Sosropartono <wsosropartono@esri.com>
2024-02-20 10:07:51 -0800

    Added ps1 scripts for testing

---
+++ build.ps1
@@ -0,0 +1,16 @@
+
+function Run{
+    start powershell {Appium}
+    start powershell {emulator -avd Pixel_4_API_30}
+    start-sleep -seconds 5
+    echo "ran"
+    
+}
+
+Run
+
+
+
+
+
+
---
+++ test.ps1
@@ -0,0 +1,10 @@
+# This will move all items from this dependencies folder over to the actual script
+# Does this have access to the envs?
+
+function Run{
+    echo "this test is being done"
+    python script.py
+}
+
+
+Run
